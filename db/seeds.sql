08c1e390f607b231206af477eb5b95e2873fbf6e
alexisn84 <91172382+alexisn84@users.noreply.github.com>
2022-01-03 12:12:58 -0500

    tables created, seeded to test

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,16 @@
-INSERT INTO department (name, description)
+INSERT INTO department (name)
 VALUES
-    ('Custodial', 'Janitorial duties for warehouses'),
-    ('Sales Representative', 'Sales team'),
-    ('Accounting', 'Oversees all bookkeeping operations');
+    ('Custodial'),
+    ('Sales'),
+    ('Accounting');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+    ('Janitor', 45000, 152),
+    ('Sales Lead', 60000, 250),
+    ('Sales Manager', 80000, 275),
+    ('A/R', 75000, 840);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES
+    ('Sally', 'Baker', 5);
--- server.js
+++ server.js
@@ -2,3 +2,26 @@
 const express = require('express');
 const db = require('./db/connection');
 const apiRoutes = require('./routes/apiRoutes');
+
+const PORT = process.env.PORT || 3001;
+const app = express()
+
+//express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use('/api', apiRoutes);
+
+//default response for other (Not Found) requests
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+
+//Listen
+db.connect(err => {
+    if (err) throw err;
+    console.log('Database connected.');
+    app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+});
