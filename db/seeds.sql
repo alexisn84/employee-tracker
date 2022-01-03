78d840550f67546df224feb101a0280fc951392b
alexisn84 <91172382+alexisn84@users.noreply.github.com>
2022-01-03 17:46:59 -0500

    api routes not needed revised code deleted routes

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,11 +6,14 @@ VALUES
 
 INSERT INTO role (title, salary, department_id)
 VALUES
-    ('Janitor', 45000, 152),
-    ('Sales Lead', 60000, 250),
-    ('Sales Manager', 80000, 275),
-    ('A/R', 75000, 840);
+    ('Janitor', 45000, 1),
+    ('Sales Lead', 60000, 2),
+    ('Sales Manager', 80000, 3),
+    ('A/R', 75000, 4);
 
-INSERT INTO employee (first_name, last_name, role_id)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
-    ('Sally', 'Baker', 5);
+    ('Sally', 'Baker', 4, null),
+    ('Shawn', 'Johnson', 2, 3),
+    ('Billy', 'Bob', 3, null),
+    ('Mary', 'Colon', 1, null);
--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 //bring dependencies
 const express = require('express');
+const res = require('express/lib/response');
 const db = require('./db/connection');
-const apiRoutes = require('./routes/apiRoutes');
+//const apiRoutes = require('./routes/apiRoutes');
+const inquirer = require('inquirer');
+const { allowedNodeEnvironmentFlags } = require('process');
+require('console.table');
 
 const PORT = process.env.PORT || 3001;
 const app = express()
@@ -9,13 +13,92 @@ const app = express()
 //express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use('/api', apiRoutes);
+//app.use('/api', apiRoutes);
+
+//connect the sql server to sql database
+db.connect (function (err) {
+    if (err) throw err;
+    firstPrompt();
+})
+
+//function allows user to to see prompt for what they like to do 
+function firstPrompt() {
+    inquirer.prompt({
+            type: 'list',
+            name: 'task',
+            message: 'Please select function you would like to do.',
+            choices: [
+                "View All Employees",
+                "View Employees by Department",
+                "Add Employee",
+                "Remove Employee",
+                "Update Employee Role",
+                "Add Role",
+                "Add Department",
+                "Exit"
+            ]
+        })
+        .then(function ({ task }) {
+            switch (task) {
+                case "View All Employees":
+                    viewEmployees();
+                    break;
+                case "View Employees by Department":
+                    viewEmployeeByDepartment();
+                    break;
+                case "Add Employee":
+                    addEmployee();
+                    break;
+                case "Remove Employees":
+                    removeEmployees();
+                    break;
+                case "Update Employee Role":
+                    updateEmployeeRole();
+                    break;
+                case "Add Role":
+                    addRole();
+                    break;
+                case "Add Department":
+                    addDepartment();
+                    break;
+                case "Exit":
+                    db.end();
+                    break;
+            }
+        });
+}
+
+//view all employees
+function viewEmployees() {
+    let query = 
+    `SELECT 
+        employee.id, 
+        employee.first_name, 
+        employee.last_name, 
+        role.title, 
+        department.name AS department, 
+        role.salary, 
+        CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+    FROM employee
+    LEFT JOIN role
+        ON employee.role_id = role.id
+    LEFT JOIN department
+        ON department.id = role.department_id
+    LEFT JOIN employee manager
+        ON manager.id = employee.manager_id`
+
+    db.query(query, (err, res)=>{
+        if (err) throw err;
+        console.table(res);
+        firstPrompt();
+    });
+}
+
 
 //default response for other (Not Found) requests
 app.use((req, res) => {
     res.status(404).end();
 });
-
 
 //Listen
 db.connect(err => {
